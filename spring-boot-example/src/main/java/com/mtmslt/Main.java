fab7380ec60b500546b3536a79419e34841deff2
Matome <seletematome3@gmail.com>
2023-04-19 15:47:46 +0200

    stringboot not running 💔

---
+++ spring-boot-example/src/main/java/com/mtmslt/Main.java
@@ -0,0 +1,12 @@
+package com.mtmslt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Main {
+
+    @SpringBootApplication
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
